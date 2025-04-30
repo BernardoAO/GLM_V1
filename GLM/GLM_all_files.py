df4204f2df731e896b092f7bcc327f89d29c3412
Bernardo Andrade Ortega <96739216+BernardoAO@users.noreply.github.com>
2025-04-30 12:38:46 +0200

    Update to include visual stimulus

--- GLM/GLM_all_files.py
+++ GLM/GLM_all_files.py
@@ -11,9 +11,8 @@ working_dir = '\\\\wsl.localhost\\Ubuntu\\home\\ag-schmitz-nwfz\\yota\\project'#
 # Session information
 sampling_rate = 30000 # Hz
 camara_sampling_rate = 200 # Hz
+rate_ratio = 150 # sampling_rate / camara_sampling_rate
 time_bin_width = 0.005 # s
-diff_TTL_usual_value = 150 # sampling_rate / camara_sampling_rate
-n_cam_switch = 9 # number of camara switches for each session
 n_ROIs = 6
 ROI_names = ["Wheel", "Vibrissae", "Nose", "Pupil", "Mouth", "Paw"]
 
@@ -22,6 +21,7 @@ GLM_type = "binary_binary"
 coef_save_path =  \
     os.path.join(working_dir, 'GLM', 'output_files', 'GLM_coef.npy')
 all_GLM_coef = np.load(coef_save_path, allow_pickle=True).item() #{}
+all_GLM_coef = {}
 
 sig_save_path =  \
     os.path.join(working_dir, 'GLM', 'output_files', 'GLM_sig.npy')
@@ -32,13 +32,15 @@ all_awake_periods = np.load(os.path.join(working_dir,'GLM',
                                          'output_files', 'valid_periods.npy'), 
                             allow_pickle=True).item()
 
-""" Old threshold code
-all_thresholds = np.load(os.path.join(working_dir,'GLM',
-                                      'output_files','manual_thresholds.npy'),
-                         allow_pickle=True).item()
-    and not all_thresholds[exp[:10]][1] == 0
-        thresholds = [th / 3 for th in all_thresholds[exp[:10]]]
-"""
+# # Vision
+# visual_stim_types = ["Sl36x22_d_3",  "Sd36x22_l_3","chirp",
+#                     "csd","cm_18x11_1", "mb","mg_sq"]
+# visual_windows = np.array([[0.05, 0.1],[0.05, 0.1],
+#                            [0.05, 35],[0.05, 1],
+#                            [0.05, 0.1],[0.05, 1.7],
+#                            [0.05,1.7]])
+
+
 
 # Get data
 path_2_spike_bundle =  \
@@ -48,71 +50,165 @@ experiments = [exp for exp in os.listdir(path_2_spike_bundle)
                if exp[:2] == "20" and exp[:10] in all_awake_periods]
 experiments.sort()
 
+"""experiments =  [#'2021-11-18_15-39-45',
+                #'2021-11-19_15-11-14',
+                # '2022-11-08_11-35-42', SC
+                # '2022-11-09_15-24-08', SC
+                '2022-12-20_15-08-10',
+                '2022-12-21_13-09-10',
+                # '2023-02-23_08-57-20',
+                # '2023-03-15_11-05-00',
+                # '2023-03-15_15-23-14',
+                # '2023-03-16_12-16-07',
+                '2023-03-21_16-17-18',
+                '2023-03-22_12-22-12',
+                '2023-04-13_12-35-02',
+                '2023-04-14_11-48-04',
+                '2023-04-17_12-26-07',
+                '2023-04-18_12-10-34',
+                #'2023-08-10_13-07-52',
+                #'2023-08-10_16-32-27',
+                #'2023-08-11_12-23-01'
+                ]"""
+
 # file loop
+unit_colors_storage = dict()
 for exp in tqdm(experiments, desc="Files processed"):
-    if  True: # exp == '2023-03-21_16-17-18': #
-        try:
+    try:
+        
+        ## IMPORT 
+        
+        visual_stim_types = ["Sd36x22_l_3","Sl36x22_d_3", "chirp",
+                            "cm_18x11_1","csd", "mb","mg","mg_sq"]
+        visual_windows = np.array([[0.05, 0.1],[0.05, 0.1],
+                                   [0.05, 35],[0.05, 0.1],
+                                   [0.05, 1],[0.05, 1.7],
+                                   [0.05, 1.7],[0.05, 1.7]
+                                   ])
+        
+        # spike data
+        Spke_Bundle, spiketimes, camera_change_times, SIN_data = \
+            hf.import_spike_data(exp, working_dir, path_2_spike_bundle)
+        
+        ## behavior
+        Behavior, running_band = hf.import_behavior(exp, working_dir)
+        
+        # vision
+        
+        #we here prepare a alternative visual_stim_types based on the stim present
+        vis_stim_local = Spke_Bundle["events"].keys()
+        
+        visual_stim_types_local = list(set(visual_stim_types) & set(vis_stim_local))
+        visual_stim_types_local = sorted(visual_stim_types_local)
+        
+        visual_windows = visual_windows[0:len(visual_stim_types_local)]
+        
+        visual_times = [(Spke_Bundle["events"][type] - 
+                         Spke_Bundle["Synchronization_TTLs"]["Sync_cam"][0]) 
+                        / sampling_rate
+                        for type in visual_stim_types_local]
+    
+        ## GLM PIPELINE
+        
+        # Get valid units and unit type
+        valid_cluster_indx, cluster_type, unit_colors = \
+            hf.get_valid_cluster(Spke_Bundle, SIN_data)            
+        valid_spiketimes = [spiketimes[i] for i in valid_cluster_indx]
+        
+        # Get times for each valid period in the session
+        awake_periods = all_awake_periods[exp[:10]]
+        start_behavior, duration_of_period = \
+            hf.get_period_times(Behavior, awake_periods, camera_change_times)
+        
+        # Get spiketrain, behavior, and visual input of each period
+        spike_counts_all_periods, behavior_array, thresholds, visual_array = \
+            hf.extract_periods(valid_spiketimes, awake_periods, start_behavior, 
+                            duration_of_period, Behavior, visual_times, 
+                            visual_windows)
+        
+        # GLM
+
+        binary_behavior = hf.get_binary_behavior(behavior_array)
+        if exp == '2023-04-13_12-35-02':
+            visual_array = visual_array[:,0:binary_behavior.shape[0]]
             
-            ## IMPORT SPIKE DATA
-            Spke_Bundle, spiketimes, camera_change_times, SIN_data = \
-                hf.import_spike_data(exp, working_dir, path_2_spike_bundle)
-            
-            ## IMPORT BEHAVIOR DATA
-            Behavior = hf.import_behavior(exp, working_dir)
-            
-            ## GLM PIPELINE
-            awake_periods = all_awake_periods[exp[:10]]
-            
-            # Get valid units and unit type
-            valid_cluster_indx, cluster_type, unit_colors = \
-                hf.get_valid_cluster(Spke_Bundle, SIN_data)            
-            valid_spiketimes = [spiketimes[i] for i in valid_cluster_indx]
-            
-            # Get times for each valid period in the session
-            start_behavior, duration_of_period = \
-                hf.get_period_times(Behavior, awake_periods, camera_change_times)
-            
-            # Get spiketrain of each period
-            spike_counts_all_periods = []
-            behavior_array = []
-            for i, period in enumerate(awake_periods):
-            
-                _, spike_counts = hf.get_spike_counts(valid_spiketimes, 
-                                                   start_behavior[i], 
-                                                   duration_of_period[i])
-                spike_counts_all_periods.append(spike_counts)
-            
-                behavior_array.append(np.array(Behavior[period]['motion'][1:]).T) #0 indx empty
-            
-            behavior_array = np.concatenate(behavior_array, axis = 0)
-            spike_counts_all_periods = np.concatenate(spike_counts_all_periods, axis = 1)
-            thresholds = hf.get_thresholds(behavior_array)
-            
-            """
-            # GLM
-            
-            GLM_coefs = hf.GLM(behavior_array, spike_counts_all_periods,
-                            GLM_type, thresholds)
-            all_GLM_coef[exp[:10]] = GLM_coefs
-            
-            # plot
-            hf.plot_activity(behavior_array, spike_counts_all_periods, unit_colors,
-                          ROI_names, exp, thresholds)
-            costume_bins = np.arange(-0.55, 0.65, 0.1)
-            hf.plot_histogram_of_GLM(GLM_coefs, ROI_names, unit_colors,
-                                  bins = costume_bins, exp = exp)
-            
-            """
-            # Significance
-            GLM_coefs = all_GLM_coef[exp[:10]]
-            sig_GLM_coef[exp[:10]] = \
-                hf.get_significance(GLM_coefs, behavior_array, spike_counts_all_periods,
-                                 GLM_type, thresholds, n_perm = 50)
-            
-            
-        except:
-            tqdm.write(" Error when processing file " + exp[:10])
+        GLM_regresors = np.concatenate((binary_behavior, visual_array.T), 
+                                       axis = 1)
+        GLM_coefs = hf.GLM(GLM_regresors, spike_counts_all_periods,
+                        GLM_type, thresholds, n_core = 4)
+        all_GLM_coef[exp[:10]] = GLM_coefs
+        
+        ## PLOT
+        
+        hf.plot_activity(behavior_array, spike_counts_all_periods, unit_colors,
+                      ROI_names, exp, thresholds)
+        
+        costume_bins = np.arange(-0.7, 0.7, 0.02)
+        
+        hf.plot_histogram_of_GLM(GLM_coefs, ROI_names, unit_colors,
+                              bins = costume_bins, exp = exp)
+        
+        hf.plot_histogram_of_GLM(GLM_coefs[:,6:], visual_stim_types_local, unit_colors,
+                              bins = costume_bins, exp = exp)#, n_ROIs=7)
+        
+        unit_colors_storage[exp[0:10]] = unit_colors
+        
+        
+        # # Significance
+        # GLM_coefs = all_GLM_coef[exp[:10]]
+        # sig_GLM_coef[exp[:10]] = \
+        #     hf.get_significance(GLM_coefs, GLM_regresors, spike_counts_all_periods,
+        #                       GLM_type, thresholds, n_perm = 50)
+    
+        
+    except:
+        tqdm.write(" Error when processing file " + exp[:10])
 
 
-#np.save(coef_save_path, all_GLM_coef)
+np.save(coef_save_path, all_GLM_coef)
+np.save(coef_save_path[0:-4] + '_color.npy', unit_colors_storage)
 np.save(sig_save_path, sig_GLM_coef)
+
+loc_storage_GLM = []
+loc_storage_GLM_stat = []
+loc_storage_color = []
+
+for i,exp_loc in enumerate(all_GLM_coef.keys()):
+    if exp_loc == '2023-08-11':
+        continue
+    if i == 0:
+        loc_storage_GLM = np.array(all_GLM_coef[exp_loc])
+        # loc_storage_GLM_stat = np.array(sig_GLM_coef[exp_loc])
+        loc_storage_color = np.array(unit_colors_storage[exp_loc])
+    else:
+        loc_storage_GLM = np.append(loc_storage_GLM,np.array(all_GLM_coef[exp_loc]),axis = 0)
+        loc_storage_GLM_stat = np.append(loc_storage_GLM_stat,np.array(sig_GLM_coef[exp_loc]),axis = 0)
+        loc_storage_color = np.append(loc_storage_color,unit_colors_storage[exp_loc])
+        
+exp = 'All recordings vision'
+hf.plot_histogram_of_GLM(loc_storage_GLM[:,6:], visual_stim_types, loc_storage_color,
+                      bins = costume_bins, exp = exp)#, n_ROIs=7)
+
+exp = 'All recordings behavior'
+hf.plot_histogram_of_GLM(loc_storage_GLM[:,:], visual_stim_types, loc_storage_color,
+                      bins = costume_bins, exp = exp)#, n_ROIs=7)
+
+# exp = 'All recordings vision stat'
+# hf.plot_histogram_of_GLM(loc_storage_GLM_stat[:,6:], visual_stim_types, loc_storage_color,
+#                       bins = costume_bins, exp = exp)#, n_ROIs=7)
+
+# exp = 'All recordings behavior stat'
+# hf.plot_histogram_of_GLM(loc_storage_GLM_stat[:,:], visual_stim_types, loc_storage_color,
+#                       bins = costume_bins, exp = exp)#, n_ROIs=7)
+
+#now we do the related scatter plot
+
+
+
+""" Old threshold code
+all_thresholds = np.load(os.path.join(working_dir,'GLM',
+                                      'output_files','manual_thresholds.npy'),
+                         allow_pickle=True).item()
+    and not all_thresholds[exp[:10]][1] == 0
+        thresholds = [th / 3 for th in all_thresholds[exp[:10]]]
+"""
--- GLM/Helper_functions.py
+++ GLM/Helper_functions.py
@@ -36,10 +36,11 @@ def import_spike_data(exp, working_dir, path_2_spike_bundle,
     
 def import_behavior(exp, working_dir):
     """
-    Imports behavioral data from the cam0_ files
+    Imports behavioral data from the cam0_ files and the running band data.
     """
     # get all cam files of the day
     cam_files_name = 'cam0_' + exp[:10]
+
     path_2_behavior =  \
         os.path.join(working_dir, 'Facemap/facemap_motion_data')
     cam_files = [file for file in os.listdir(path_2_behavior) 
@@ -50,8 +51,18 @@ def import_behavior(exp, working_dir):
     for file in cam_files:
         Behavior.append(np.load(os.path.join(path_2_behavior,file)
                 ,allow_pickle=True,encoding='latin1').item())
-        
-    return Behavior
+    
+    # get running band data
+    path_2_run =  \
+        os.path.join(working_dir, 'storage_run_simple')
+    file = exp + "_local_simple_run.npy"
+
+    if file in os.listdir(path_2_run):
+        running_band = np.load(os.path.join(path_2_run,file), allow_pickle=True,encoding='latin1')
+    else:
+        running_band = []
+
+    return Behavior, running_band
 
 def get_valid_cluster(Spke_Bundle, SIN_data):
     """
@@ -143,6 +154,135 @@ def get_spike_counts(spike_times, start = 0, duration = 1000, time_bin_width = 0
     
     return tv, binary_st
 
+def get_visual_input(visual_times, windows, start = 0, duration = 1000, time_bin_width = 0.005):
+    """
+    Gets a matrix of n_stimtypes x period_time aligned to the start of the period with the spike counts for fixed windows.
+    
+    Parameters:
+    - visual_times: List of input types containing a list of the event times.
+    - windows: start and end [seconds] of a time window to use for each stim type. shape: n_types x 2
+    - start: first time in the period relative to the Spike times [seconds]
+    - duration: duration of the period [seconds] 
+    - time_bin_width: width of the time bin (step size)
+
+    Returns:
+    - tv: time vector
+    - binary_st: Matrix of binarized spikes n_units x time
+    """
+
+    tv = np.arange(0, duration, time_bin_width)
+    binary_stim = np.zeros((len(visual_times), len(tv)))
+    
+    for n in range(len(visual_times)):
+
+        aligned_visual_times = np.array(visual_times[n]) - start
+        all_times_indx = aligned_visual_times // time_bin_width
+        indx = all_times_indx[(all_times_indx > 0) & 
+                               (all_times_indx < len(tv))]
+        window_indx = windows[n,:] // time_bin_width
+        joint_indx = np.array([np.arange(i + window_indx[0], i + window_indx[1])
+                                   for i in indx]).flatten().astype("uint64")
+        binary_stim[n, joint_indx[joint_indx < len(tv)]] = 1
+    
+    return tv, binary_stim
+
+def join_running_band(behavior_no_rb, running_band, sync, rate_ratio=150):
+    """
+    Substitutes the Wheel cam channel (first channel of the input array) for the running band signal,
+    in the case there is one
+    
+    Parameters:
+    - behavior_no_rb: original behavior array, shape time x n_ROIs
+    - running_band: shape t
+    - sync: syncronization index 
+    - rate_ratio: ratio between the running band and cam signals
+
+    Returns:
+    - behavior_rb: behavior array with the running band signal, shape time x n_ROIs
+    """
+    # Import
+    
+    if len(running_band) > 0:
+        lenb = behavior_no_rb.shape[0]
+        behavior_rb = np.copy(behavior_no_rb)
+
+        running_band_sub_sampled = running_band[::rate_ratio]
+        behavior_rb[:,0] = running_band_sub_sampled[sync:sync+lenb]
+
+        return behavior_rb
+
+    else:
+        return behavior_no_rb
+    
+    '''
+    # plot current
+        fig, ax1 = plt.subplots()
+        t = np.arange(0,lenb/200,1/200)
+        color = 'tab:red'
+        ax1.set_xlabel('time (s)')
+        ax1.set_ylabel('cam', color=color)
+        ax1.plot(t, behavior_no_rb[:,0], color=color)
+        ax1.tick_params(axis='y', labelcolor=color)
+
+        ax2 = ax1.twinx()  
+        color = 'tab:blue'
+        ax2.set_ylabel('running_band', color=color) 
+        ax2.plot(t, behavior_rb[:,0], color=color, alpha=0.7)
+        ax2.tick_params(axis='y', labelcolor=color)
+
+        fig.tight_layout()
+        plt.show()
+    # plot old
+    plt.plot(Behavior[0]['motion'][1]/np.max(Behavior[0]['motion'][1]))
+            
+            start_run = Spke_Bundle["Synchronization_TTLs"]["Sync_cam"][0]
+            # 94 943 605
+            lenb = Behavior[0]['motion'][1].size
+            s = start_run // diff_TTL_usual_value #int(1e6)
+            
+            rb_s = running_band[s:s+lenb*diff_TTL_usual_value]
+            rb = rb_s[::150] - running_band[0]
+            plt.plot(rb/np.max(rb), alpha=0.8)
+            plt.show()
+    '''
+
+def extract_periods(valid_spiketimes, awake_periods, start_behavior, duration_of_period,
+                    Behavior, visual_times, visual_windows, camara_sampling_rate = 200):
+    """
+    Aligns and merges the spike, behavior, and visual time signals into periods defined by the times
+    the camara was active, respectively.
+    """
+    spike_counts_all_periods = []
+    behavior_array = []
+    visual_array = []
+    for i, period in enumerate(awake_periods):
+        # spikes
+        _, spike_counts = get_spike_counts(valid_spiketimes, 
+                                            start_behavior[i], 
+                                            duration_of_period[i])
+        spike_counts_all_periods.append(spike_counts)
+        
+        # behavior
+        behavior_no_rb = np.array(Behavior[period]['motion'][1:]).T #0 indx empty
+        sync = int(6666 + start_behavior[i]*camara_sampling_rate)
+        behavior_rb = join_running_band(behavior_no_rb, 
+                                            [], sync) # running_band
+        behavior_array.append(behavior_rb) 
+        
+        # visual
+        _, binary_stim = get_visual_input(visual_times, visual_windows,
+                                                start_behavior[i], 
+                                                duration_of_period[i])
+        visual_array.append(binary_stim)
+    
+    spike_counts_all_periods = np.concatenate(spike_counts_all_periods, axis = 1)
+    
+    behavior_array = np.concatenate(behavior_array, axis = 0)
+    thresholds = get_thresholds(behavior_array)
+    
+    visual_array = np.concatenate(visual_array, axis = 1)
+    return spike_counts_all_periods, behavior_array, thresholds, visual_array
+
 def get_binary_behavior(B, thresholds=[]):
     """
     Turns a SVd matrix to a binary matrix (move or not move) given a threshold
@@ -198,7 +338,7 @@ def clean_behavior(behavior_array, n_ROIs = 6, ratio_for_outlayer = 1e-3, stds_f
     return clean_behavior
 
 
-def GLM(behavior_array, spike_counts, GLM_type, thresholds = []):
+def GLM(behavior_array, spike_counts, GLM_type, thresholds = [], n_core = -1):
     """
     Fits a Generalized Linear Model (GLM).
     
@@ -219,7 +359,7 @@ def GLM(behavior_array, spike_counts, GLM_type, thresholds = []):
 
     if GLM_type == "binary_binary":
         clf = linear_model.LogisticRegression()
-        X = get_binary_behavior(behavior_array, thresholds)
+        X = behavior_array
         y = spike_counts #np.where(spike_counts >= 1, 1, 0)  
 
     elif GLM_type == "continious_binary":
@@ -238,7 +378,7 @@ def GLM(behavior_array, spike_counts, GLM_type, thresholds = []):
         return clf.coef_
 
     # Parallelize the model fitting across neurons
-    GLM_coefs = Parallel(n_jobs=-1)(delayed(fit_model_for_neuron)(n) for n in range(n_neu))
+    GLM_coefs = Parallel(n_jobs=n_core)(delayed(fit_model_for_neuron)(n) for n in range(n_neu))
 
     return np.concatenate(GLM_coefs, axis = 0)
 
@@ -361,7 +501,7 @@ def plot_activity(behavior_array, spike_counts, colors, ROI_names, exp, threshol
     plt.show()
 
 
-def plot_scatter_of_GLM(x, y, colors, ROIs_to_plot):
+def plot_scatter_of_GLM(x, y, colors, coefs_to_plot):
     plt.scatter(x, y, c = colors, alpha = 0.4, edgecolors="none")
 
     plt.xlim(-max(abs(min(x)), abs(max(x))), max(abs(min(x)), abs(max(x))))
@@ -375,33 +515,60 @@ def plot_scatter_of_GLM(x, y, colors, ROIs_to_plot):
     plt.gca().spines['right'].set_color('none')
     plt.gca().spines['top'].set_color('none')
 
-    plt.xlabel(f"{ROIs_to_plot[0]} $\\beta$", labelpad=120)
-    plt.ylabel(f"{ROIs_to_plot[1]} $\\beta$", labelpad=150)
-
+    plt.xlabel(f"{coefs_to_plot[0]} $\\beta$", labelpad=120)
+    plt.ylabel(f"{coefs_to_plot[1]} $\\beta$", labelpad=150)
+    plt.savefig('scatter' + str(coefs_to_plot)+'.pdf',dpi = 200)
     plt.show()
 
-def plot_histogram_of_GLM(GLM_coefs, ROI_names, colors, bins = np.array([]), n_ROIs = 6, exp = '2023-03-15_11-05-00'):
+def plot_histogram_of_GLM(GLM_coefs, coef_names, colors, bins = np.array([]), n_coef = 6, exp = '2023-03-15_11-05-00'):
     colors = np.array(colors)
     if bins.size == 0:
-        bins = np.linspace(np.min(GLM_coefs), np.max(GLM_coefs), 20)
+        bins = np.linspace(np.min(GLM_coefs), np.max(GLM_coefs), 50)
 
     plt.figure(figsize=(6,10))
-    for roi in range(n_ROIs):
+    for roi in range(n_coef):
         for color in np.unique(colors):
             indx =  np.argwhere(colors == color).flatten()
             
-            plt.subplot(6, 1, roi + 1)
+            plt.subplot(n_coef, 1, roi + 1)
             plt.hist(GLM_coefs[indx, roi], bins = bins, histtype='step', 
                      stacked=True, fill=False, edgecolor=color, density=True)
-            plt.title(ROI_names[roi])
+            plt.title(coef_names[roi])
 
         plt.vlines(0, 0, plt.gca().get_ylim()[1], linestyles="--", color = "black", alpha=0.5)
         plt.gca().spines['top'].set_visible(False)
         plt.gca().spines['right'].set_visible(False)
-        if roi < n_ROIs - 1:
+        if roi < n_coef - 1:
             plt.xticks([])
+        if roi == 0:
+            plt.legend(["n_TCA = " + str(np.argwhere(colors == 'purple').flatten().shape[0]),
+                        "n_NW = " + str(np.argwhere(colors == 'red').flatten().shape[0]),
+                        "n_BW = " + str(np.argwhere(colors == 'black').flatten().shape[0])])
     
     plt.suptitle("Experiment day " + exp[:10])
     plt.xlabel(f"$\\beta$")
     plt.tight_layout()
+    plt.savefig('histogram ' + str(exp) + '.pdf',dpi = 200)
     plt.show()
+
+def plot_visual(visual_times, visual_stim_types, sampling_rate = 30000):
+    colors = {
+    "chirp": "#e6194B",       # strong red
+    "Sd36x22_l_3": "#3cb44b", # green
+    "Sl36x22_d_3": "#4363d8", # blue
+    "mb": "#f58231",          # orange
+    "csd": "#911eb4",         # purple
+    "cm_18x11_1": "#46f0f0",  # cyan
+    "mg_sq": "#f032e6"        # magenta
+    }
+    for stim in visual_stim_types:
+        plt.eventplot(visual_times[stim] / sampling_rate, color=colors[stim],label=stim)
+    plt.legend()
+    plt.show()
+    """
+    for i,vtype in enumerate(visual_stim_types):
+        plt.plot(visual_array[i,:],label=vtype)
+    plt.legend()
+    plt.show()
+    """
+    
